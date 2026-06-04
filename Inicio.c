dd90f09cde4aaba3389d4e73c49d68f4ca661de2
João Pedro <joaoos2star@gmail.com>
2026-06-04 14:06:34 -0300

    Feat

--- Inicio.c
+++ Inicio.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
+#include <locale.h>
 
-int main()
-{
-    typedef struct
+typedef struct
     {
         int dia;
         int mes;
         int ano;
     } Data;
 
+int main(){
+    setlocale(LC_ALL, "Portuguese");
     Data data;
-    printf("informe uma data em formato dd/mm/aaaa\n");
-    scanf("%d/%d/%d", &data.dia, &data.mes, &data.ano);
-    printf("%d/%d/%d", data.dia, data.mes, data.ano);
+    do{
+        printf("informe uma data em formato dd/mm/aaaa\n");
+        scanf("%d/%d/%d", &data.dia, &data.mes, &data.ano);
+    }while(!validarData(data));
+
+    printf("A data é em %d/%d/%d", data.dia, data.mes, data.ano);
+}
+
+int validarData(Data data){
+    if(data.dia<=0||data.dia>31){
+        return 0;
+    }
+    if(data.mes<=0||data.mes>12){
+        return 0;
+    }
+    if(data.ano<=0){
+        return 0;
+    }
+    return 1;
 }
