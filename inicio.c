4a447d79de21cc3f28a87c86976d7a80d9b98205
João Pedro <joaoos2star@gmail.com>
2026-06-06 09:05:55 -0300

    Feat: Atualização do Validar data

--- inicio.c
+++ inicio.c
@@ -7,29 +7,51 @@ typedef struct
     int mes;
     int ano;
 } Data;
-int ehbissexto(int a)
+
+int ehbissexto(Data data)
 {
-    if ((a % 4 == 0 && a % 100 != 0) || (a % 400 == 0))
+    if ((data.ano % 4 == 0 && data.ano % 100 != 0) || (data.ano % 400 == 0))
     {
         return 1;
     }
     return 0;
 }
+
 int validarData(Data data)
 {
-    if (data.dia <= 0 || data.dia > 31)
+    if (data.mes <= 0 || data.mes > 12 || data.ano <= 0)
     {
         return 0;
     }
-    if (data.mes <= 0 || data.mes > 12)
+
+    int diasDoMes[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    if(ehbissexto(data)){
+        diasDoMes[2] = 29;
+    }
+
+    if (data.dia <= 0 || data.dia > diasDoMes[data.mes])
     {
         return 0;
     }
-    if (data.ano <= 0)
-    {
-        return 0;
-    }
+    
     return 1;
+}
+
+int menu()
+{
+    int opcao;
+
+    printf("========== ESCOLHA DO CALCULO ==========\n");
+    printf("1) Calcular a quantidade de dias entre duas datas no mesmo mês.\n");
+    printf("2) Calcular a quantidade de dias entre duas datas no mesmo ano.\n");
+    printf("3) Calcular a quantidade de dias entre duas datas em anos consecutivos.\n");
+    printf("4) Calcular a quantidade de dias entre duas datas em anos quaisquer (inclusive em séculos diferentes).\n");
+    printf("5) Calcular a data final que marca a quantidade de dias passado no periodo.\n");
+    printf("Digite um dos valores acima para escolher (1 a 5), 0 para cancelar.\n");
+    scanf("%d", &opcao);
+
+    return opcao;
 }
 
 int main()
@@ -49,9 +71,9 @@ int main()
     {
         printf("informe a segunda data em formato dd/mm/aaaa\n");
         scanf("%d/%d/%d", &data2.dia, &data2.mes, &data2.ano);
-    } while (!validarData(data));
+    } while (!validarData(data2));
 
-    printf("A data é em %d/%d/%d\n", data.dia, data.mes, data.ano);
+    printf("A primeira data é em %d/%d/%d\n", data.dia, data.mes, data.ano);
     printf("A segunda data é em %d/%d/%d\n", data2.dia, data2.mes, data2.ano);
 
     opcao = menu();
@@ -60,36 +82,27 @@ int main()
 
     case 1:
         printf("Calculando...");
+        break;
 
     case 2:
         printf("Calculando...");
+        break;
 
     case 3:
         printf("Calculando...");
+        break;
 
     case 4:
         printf("Calculando...");
+        break;
 
     case 5:
         printf("Calculando...");
+        break;
 
     default:
         printf("Invalido!");
+        break;
+        
     }
-}
-
-int menu()
-{
-    int opcao;
-
-    printf("========== ESCOLHA DO CALCULO ==========\n");
-    printf("1) Calcular a quantidade de dias entre duas datas no mesmo mês.\n");
-    printf("2) Calcular a quantidade de dias entre duas datas no mesmo ano.\n");
-    printf("3) Calcular a quantidade de dias entre duas datas em anos consecutivos.\n");
-    printf("4) Calcular a quantidade de dias entre duas datas em anos quaisquer (inclusive em séculos diferentes).\n");
-    printf("5) Calcular a data final que marca a quantidade de dias passado no periodo.\n");
-    printf("Digite um dos valores acima para escolher (1 a 5), 0 para cancelar.\n");
-    scanf("%d", &opcao);
-
-    return opcao;
 }
