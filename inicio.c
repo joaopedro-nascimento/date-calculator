8d8deba0bbc80b39f2dfd7bb380b8ab42a1df7b1
João Pedro <joaoos2star@gmail.com>
2026-06-13 16:24:35 -0300

    Feat: Ao final de cada operação, é dada a opção para o usuário fazer nova operação (ou não) com um novo valor

--- inicio.c
+++ inicio.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <locale.h> 
+#include <locale.h>
 #include <stdlib.h>
 
 int diasDoMes[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
@@ -93,42 +93,45 @@ int dma(Data data, Data data2)
     return soma - somaDia;
 }
 
-int DmAC(Data data, Data data2){
+int DmAC(Data data, Data data2)
+{
     int soma1 = 0;
     int soma2 = 0;
     int total;
 
     diasDoMes[2] = ehbissexto(data) ? 29 : 28;
     soma1 = (diasDoMes[data.mes] - data.dia);
-    for(int i = data.mes + 1; i <= 12; i++){
+    for (int i = data.mes + 1; i <= 12; i++)
+    {
         soma1 += diasDoMes[i];
     }
 
     diasDoMes[2] = ehbissexto(data2) ? 29 : 28;
-    for(int i = 1; i < data2.mes; i++){
+    for (int i = 1; i < data2.mes; i++)
+    {
         soma2 += diasDoMes[i];
     }
     soma2 += data2.dia;
     total = soma1 + soma2;
 
-    if(data.ano > data2.ano){
+    if (data.ano > data2.ano)
+    {
         total = -total;
     }
 
     return total;
 }
 
-long converterParaDias(Data data){
+long converterParaDias(Data data)
+{
     long total = 0;
     int anosAnteriores = data.ano - 1;
 
-    total = (long)anosAnteriores * 365
-    + (anosAnteriores / 4)
-    - (anosAnteriores / 100)
-    + (anosAnteriores / 400);
+    total = (long)anosAnteriores * 365 + (anosAnteriores / 4) - (anosAnteriores / 100) + (anosAnteriores / 400);
 
     diasDoMes[2] = ehbissexto(data) ? 29 : 28;
-    for (int i = 1; i < data.mes; i++){
+    for (int i = 1; i < data.mes; i++)
+    {
         total += diasDoMes[i];
     }
 
@@ -137,30 +140,37 @@ long converterParaDias(Data data){
     return total;
 }
 
-int DmAQ(Data data, Data data2){
+int DmAQ(Data data, Data data2)
+{
     long dias1 = converterParaDias(data);
     long dias2 = converterParaDias(data2);
 
     return labs(dias2 - dias1);
 }
 
-Data descobrirData(long total){
+Data descobrirData(long total)
+{
     Data dataResultado;
     dataResultado.ano = 1;
 
-    while(1){
+    while (1)
+    {
         int diasDesteAno = ehbissexto(dataResultado) ? 366 : 365;
-        if(total > diasDesteAno){
+        if (total > diasDesteAno)
+        {
             total -= diasDesteAno;
             dataResultado.ano++;
-        } else{
+        }
+        else
+        {
             break;
         }
     }
     diasDoMes[2] = ehbissexto(dataResultado) ? 29 : 28;
     dataResultado.mes = 1;
 
-    while(total > diasDoMes[dataResultado.mes]){
+    while (total > diasDoMes[dataResultado.mes])
+    {
         total -= diasDoMes[dataResultado.mes];
         dataResultado.mes++;
     }
@@ -183,96 +193,108 @@ int main()
     long DiferDmAQ;
     int diasParaSomar;
 
+    int continuar;
     do
     {
-        printf("informe a primeira data em formato dd/mm/aaaa\n");
-        scanf("%d/%d/%d", &data.dia, &data.mes, &data.ano);
-        if (!validarData(data))
+        do
         {
-            printf("Data inválida!\n");
-        }
-    } while (!validarData(data));
-
-    do
-    {
-        printf("informe a segunda data em formato dd/mm/aaaa\n");
-        scanf("%d/%d/%d", &data2.dia, &data2.mes, &data2.ano);
-        if (!validarData(data2))
-        {
-            printf("Data inválida!\n");
-        }
-    } while (!validarData(data2));
-
-    printf("A primeira data é em %d/%d/%d\n", data.dia, data.mes, data.ano);
-    printf("A segunda data é em %d/%d/%d\n", data2.dia, data2.mes, data2.ano);
-
-    opcao = menu();
-    switch (opcao)
-    {
-
-    case 1:
-        if (data.mes != data2.mes)
-        {
-            printf("Erro: As datas precisam ser do mesmo mês para esta opção!\n");
-        }
-        else
-        {
-            printf("Calculando...\n");
-            DiferDmM = DmM(data, data2);
-            printf("A quantidade de dias entre as duas datas eh de %d dias.\n", DiferDmM);
-        }
-    break;
-
-    case 2:
-        if (data.ano != data2.ano)
-        {
-            printf("Sinto muito! Mas esta opcão só é válida para datas dentro do mesmo ano...\n");
-        }
-        else
-        {
-            DiferDmA = dma(data, data2);
-            printf("Calculando...\n");
-            printf("A quantidade de dias entre as duas datas é de %d dias.\n", DiferDmA);
-        }
-    break;
-
-    case 3:
-        printf("Calculando...\n");
-        if (data2.ano - data.ano != 1){
-            printf("Erro: As data devem ser de anos consecutivos para essa opção.");
-        } else{
-        DiferDmAC = DmAC(data, data2);
-        printf("A quantidade de dias entre as duas datas é de %d dias.\n", DiferDmAC);
-        }
-    break;
-
-    case 4:
-        printf("Calculando...\n");
-        DiferDmAQ = DmAQ(data, data2);
-        printf("A quantidade de dias entre as duas datas eh de %ld dias\n", DiferDmAQ);
-    break;
-
-    case 5:
-        do{
-            printf("Digite a quantidade de dias que se passaram no periodo: ");
-            scanf("%d", &diasParaSomar);
-            if(diasParaSomar < 0){
-                printf("Por favor insira uma quantidade positiva de dias.");
+            printf("informe a primeira data em formato dd/mm/aaaa\n");
+            scanf("%d/%d/%d", &data.dia, &data.mes, &data.ano);
+            if (!validarData(data))
+            {
+                printf("Data inválida!\n");
             }
-        } while(diasParaSomar < 0);
+        } while (!validarData(data));
 
-        printf("Calculando...\n");
+        do
+        {
+            printf("informe a segunda data em formato dd/mm/aaaa\n");
+            scanf("%d/%d/%d", &data2.dia, &data2.mes, &data2.ano);
+            if (!validarData(data2))
+            {
+                printf("Data inválida!\n");
+            }
+        } while (!validarData(data2));
 
-        long diasData1 = converterParaDias(data);
-        long diasFinal = diasData1 + diasParaSomar;
-        dataFinal = descobrirData(diasFinal);
+        printf("A primeira data é em %d/%d/%d\n", data.dia, data.mes, data.ano);
+        printf("A segunda data é em %d/%d/%d\n", data2.dia, data2.mes, data2.ano);
 
-        printf("A data final apos %d dias sera: %02d/%02d/%04d\n", diasParaSomar, dataFinal.dia, dataFinal.mes, dataFinal.ano);
-    break;
+        opcao = menu();
+        switch (opcao)
+        {
 
-    default:
-        printf("Invalido!");
-    break;
-    }
+        case 1:
+            if (data.mes != data2.mes)
+            {
+                printf("Erro: As datas precisam ser do mesmo mês para esta opção!\n");
+            }
+            else
+            {
+                printf("Calculando...\n");
+                DiferDmM = DmM(data, data2);
+                printf("A quantidade de dias entre as duas datas eh de %d dias.\n", DiferDmM);
+            }
+            break;
+
+        case 2:
+            if (data.ano != data2.ano)
+            {
+                printf("Sinto muito! Mas esta opcão só é válida para datas dentro do mesmo ano...\n");
+            }
+            else
+            {
+                DiferDmA = dma(data, data2);
+                printf("Calculando...\n");
+                printf("A quantidade de dias entre as duas datas é de %d dias.\n", DiferDmA);
+            }
+            break;
+
+        case 3:
+            printf("Calculando...\n");
+            if (data2.ano - data.ano != 1)
+            {
+                printf("Erro: As data devem ser de anos consecutivos para essa opção.");
+            }
+            else
+            {
+                DiferDmAC = DmAC(data, data2);
+                printf("A quantidade de dias entre as duas datas é de %d dias.\n", DiferDmAC);
+            }
+            break;
+
+        case 4:
+            printf("Calculando...\n");
+            DiferDmAQ = DmAQ(data, data2);
+            printf("A quantidade de dias entre as duas datas eh de %ld dias\n", DiferDmAQ);
+            break;
+
+        case 5:
+            do
+            {
+                printf("Digite a quantidade de dias que se passaram no periodo: ");
+                scanf("%d", &diasParaSomar);
+                if (diasParaSomar < 0)
+                {
+                    printf("Por favor insira uma quantidade positiva de dias.");
+                }
+            } while (diasParaSomar < 0);
+
+            printf("Calculando...\n");
+
+            long diasData1 = converterParaDias(data);
+            long diasFinal = diasData1 + diasParaSomar;
+            dataFinal = descobrirData(diasFinal);
+
+            printf("A data final apos %d dias sera: %02d/%02d/%04d\n", diasParaSomar, dataFinal.dia, dataFinal.mes, dataFinal.ano);
+            break;
+
+        default:
+            printf("Invalido!");
+            break;
+        }
+        printf("\nDeseja realizar uma nova operacao? (1 - Sim / 0 - Nao): ");
+        scanf("%d", &continuar);
+
+    } while (continuar == 1);
     return 0;
 }
